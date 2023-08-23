d404f713dad96a19e14794cb59bb93df12c79019
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-22 20:30:51 -0600

    Add all required routes

--- app.js
+++ app.js
@@ -119,10 +119,69 @@ function(accessToken, refreshToken, profile, cb) {
 const Email = mongoose.model("Email", emailSchema);
 
 
-//Handle Manual Registration
+////////////////Handle Manual Registration
+//Handle Manual Registration and implement input validation on the register route
+app.post(
+  '/register',
+  [
+    body('username').notEmpty().withMessage('Username is required'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    // Proceed with registration logic
+    User.register(
+      { username: req.body.username, email: req.body.email },
+      req.body.password,
+      function (err, user) {
+        if (err) {
+          if (err.name === 'UserExistsError') {
+            return res.status(400).json({ error: 'Username or email already exists' });
+          }
 
+          console.error('Error during registration:', err);
+          return res.status(500).json({ error: 'An error occurred during registration' });
+        }
+        
+        // User registration successful, authenticate using Passport
+        passport.authenticate('local')(req, res, function () {
+          if (req.xhr) {
+            // If the request is an AJAX request (JSON request)
+            res.status(200).json({ message: 'Registration and authentication successful' });
+          } else {
+            // If the request is not an AJAX request (regular browser request)
+            res.redirect('/dashboard'); // Replace '/dashboard' with the actual dashboard route
+          }
+        });
+      }
+    );
+  }
+);
 
+//Handle manual login and implement on the login route
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    email:req.body.email,
+    password: req.body.password
+  });
+  
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("/login"); // Redirect to login page on error
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/dashboard"); // Redirect to secrets page after successful login
+      });
+    }
+  });
+});
 
 
 //Define Google authentication routes
@@ -180,6 +239,30 @@ app.get("/", (req, res) => {
     }
   });
 
+//Add post request to save emails
+app.post('/compose', (req, res) => {
+  const { recipient, subject, body } = req.body;
+  // Create a new Email document and save it to the database
+  const newEmail = new Email({
+    sender: req.user, // Assuming req.user contains the authenticated user
+    recipient,
+    subject,
+    body,
+    scheduledAt: new Date(), // You can set a default value or adjust as needed
+  });
+
+  newEmail.save()
+    .then(() => {
+      // Email saved successfully
+      res.redirect('/dashboard'); // Redirect to dashboard or wherever you want
+    })
+    .catch(err => {
+      // Error saving email
+      console.error('Error saving email:', err);
+      res.status(500).json({ error: 'An error occurred while saving the email' });
+    });
+});
+
 app.get("/login", (req, res) => {
     res.render("login")
   });
@@ -208,7 +291,7 @@ app.get("/register", (req, res) => {
 
 
 
-  app.post("/logout", function(req, res) {
+app.post("/logout", function(req, res) {
     req.logout(function(err) {
       if (err) {
         console.error("Logout error:", err);
