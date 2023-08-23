4e02a501eeb12a72ae7a8054922428da21393fc0
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-22 19:05:16 -0600

    Fix stylying, authenticate with google, render certain pages for authenticated users

--- app.js
+++ app.js
@@ -85,20 +85,99 @@ const userSchema = new mongoose.Schema({
     scheduledAt: Date,
   });
   
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
+    });
+});
+
+//Authenticate with Google
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/compose"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
   // Create the Email model
-  const Email = mongoose.model("Email", emailSchema);
+const Email = mongoose.model("Email", emailSchema);
 
-// Define your routes
+
+//Handle Manual Registration
+
+
+
+
+
+//Define Google authentication routes
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/compose', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+  app.get('/auth/google/schedule', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+  app.get('/auth/google/sent', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+  app.get('/auth/google/dashboard', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/dashboard');
+  });
+
+
+// Define  routes
 app.get("/", (req, res) => {
-    res.render("home")
+    res.render("register")
   });
 
-app.get("/home", (req, res) => {
-    res.render("home")
+  //Render dashboard for authenticated users
+  app.get("/dashboard", function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("dashboard"); // Render the "dashboard" template
+    } else {
+      res.redirect("/login"); // Redirect to login page if user is not authenticated
+    }
   });
-
-app.get("/compose", (req, res) => {
-    res.render("compose")
+  //Render compose page for authenticated users
+  app.get("/compose", function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("compose"); // Render the "compose" template
+    } else {
+      res.redirect("/login"); // Redirect to login page if user is not authenticated
+    }
   });
 
 app.get("/login", (req, res) => {
@@ -109,14 +188,41 @@ app.get("/register", (req, res) => {
     res.render("register")
   });
 
-  app.get("/schedule", (req, res) => {
-    res.render("schedule")
+//Render schedule page for authenticated users
+  app.get("/schedule", function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("schedule"); // Render the "schedule" template
+    } else {
+      res.redirect("/login"); // Redirect to login page if user is not authenticated
+    }
   });
 
-  app.get("/sent", (req, res) => {
-    res.render("sent")
+//Render sent page for authenticated users
+  app.get("/sent", function(req, res) {
+    if (req.isAuthenticated()) {
+      res.render("sent"); // Render the "sent" template
+    } else {
+      res.redirect("/login"); // Redirect to login page if user is not authenticated
+    }
   });
 
+
+
+  app.post("/logout", function(req, res) {
+    req.logout(function(err) {
+      if (err) {
+        console.error("Logout error:", err);
+        return res.redirect("/"); // Redirect to home page in case of error
+      }
+      res.redirect("/");
+    });
+  });
+  
+  
+  
+  
+  
+  
   app.listen(PORT, () => {
     console.log(`App is up and listening on port ${PORT}`);
   });
